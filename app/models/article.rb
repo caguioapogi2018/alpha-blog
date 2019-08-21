dcac44764fb89d06f5f38a7da241db75af44305b
Glenn <glenngwapo12@gmail.com>
2019-08-21 11:22:03 +0800

    alpha-blog create table, model and validations

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ActiveRecord::Base
+
+  validates :title, presense:true, length: { minimun: 3, maximum: 50}
+  validates :description, presense:true, length: { minimum: 3, maximum: 500}
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,5 @@ Rails.application.routes.draw do
   get 'pages/about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
