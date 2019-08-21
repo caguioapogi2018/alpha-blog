dfbe3e5f3a8a6570f105f63e8aab324ebcdb7e65
Glenn <glenngwapo12@gmail.com>
2019-08-21 18:36:48 +0800

    Add partials and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,45 +1,55 @@
 class ArticlesController < ApplicationController
 
   def index
-    @articles = Article.all
+          @articles = Article.all
   end
 
   def new
-    @article = Article.new
+          @article = Article.new
   end
 
   def create
-    @article = Article.new(article_params)
-    if @article.save
-      flash[:notice] = "Article was successfully created"
-      redirect_to article_path(@article)
-    else
-      render 'new'
-    end
+          @article = Article.new(article_params)
+            if @article.save
+               flash[:notice] = "This article was successfully Created"
+                redirect_to article_path(@article)
+            else
+              render 'new'
+            end
 
   end
 
   def show
-    @article = Article.find(params[:id])
+            @article = Article.find(params[:id])
+  end
+
+  def destroy
+            @article = Article.find(params[:id])
+            @article.destroy
+                  flash[:notice] = "This article was successfully Deleted"
+                  redirect_to articles_path
   end
 
   def edit
-    @article = Article.find(params[:id])
+            @article = Article.find(params[:id])
   end
 
+
   def update
-    @article = Article.find(params[:id])
-    if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
-      redirect_to article_path(@article)
-    else
-      render 'edit'
-    end
+            @article = Article.find(params[:id])
+              if @article.update(article_params)
+                  flash[:notice] = "This article was successfully Updated"
+                   redirect_to article_path(@article)
+              else
+                  render 'edit'
+              end
   end
+
+
 
   private
   def article_params
-    params.require(:article).permit(:title, :description)
+            params.require(:article).permit(:title, :description)
   end
 
 
