4604352db5479cee0105284c3a1c41f73b3d59bb
Glenn <glenngwapo12@gmail.com>
2019-08-21 13:34:21 +0800

    this is create new template, action and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,7 @@
 class Article < ActiveRecord::Base
 
-  validates :title, presense:true, length: { minimun: 3, maximum: 50}
-  validates :description, presense:true, length: { minimum: 3, maximum: 500}
-
+  #validates :title, presense:true, length: { minimun: 3, maximum: 50}
+  #validates :description, presense:true, length: { minimum: 3, maximum: 500}
 
 
 end
